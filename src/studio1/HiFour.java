a9b8a1a8c70d0c876fce5c6c2a851803f3676b10
nguye <nguye@LAPTOP-L3HDNCPB.dhcp.wustl.edu>
2023-09-06 13:41:42 -0500

    Studio 1 is done!

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -9,17 +9,23 @@ import java.util.Scanner;
 public class HiFour {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter value for arg 0: ");
+		System.out.print("Please enter the name of first member: ");
 		String s0 = in.nextLine();
-		System.out.print("Enter value for arg 1: ");
+		System.out.print("Please enter the name of second member: ");
 		String s1 = in.nextLine();
-		System.out.print("Enter value for arg 2: ");
+		System.out.print("Please enter the name of third member: ");
 		String s2 = in.nextLine();
-		System.out.print("Enter value for arg 3: ");
+		System.out.print("Please enter the name of fourth member: ");
 		String s3 = in.nextLine();
+		System.out.print("Greetings " + s0 + ", " + s1 + ", " + s2 + ", and " + s3);
 		//
 		// Say hello to the names in s0 through s3.
 		//
+		//Please enter the name of first member: Sun
+		//Please enter the name of second member: Patino
+		//Please enter the name of third member: Nguyen
+		//Please enter the name of fourth member: Quanze
+		//Greetings Sun, Patino, Nguyen, and Quanze
 
 	}
 }
